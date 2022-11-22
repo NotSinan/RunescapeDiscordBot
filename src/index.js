fc6b525d4ddf2194736ec5cd644bc5d5ca84f175
Sinan <haxsmith77@gmail.com>
2022-11-23 00:17:46 +1100

    Added command handler and deploy commands functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
+// Require the necessary discord.js classes
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, interaction => {
-	console.log(interaction);
-});
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
@@ -24,6 +22,29 @@ for (const file of commandFiles) {
 	}
 }
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = interaction.client.commands.get(interaction.commandName);
 
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
 client.login(token);
