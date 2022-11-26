3f0e0c559cfaf5d5618fef82db1d5814add9b62f
Sinan <haxsmith77@gmail.com>
2022-11-27 00:49:52 +1100

    Updated grandexchange.js

--- src/commands/grandexchange.js
+++ src/commands/grandexchange.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,24 +12,33 @@ module.exports = {
     ),
 
     async execute(interaction) {
-        const itemId = interaction.options.getInteger('itemID')
+        const itemId = interaction.options.getInteger('item')
+        console.log(itemId)
+
+        const itemData = await fetch(`https://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=${itemId}`)
+        .then((response) => response.json())
+
+        console.log(`${itemData.item.name}`)
+        
+
         const itemEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+        .setTitle(`${itemData.item.name}`)
+        .setURL(`https://secure.runescape.com/m=itemdb_rs/viewitem?obj=${itemData.item.id}`)
+        .setDescription(`${itemData.item.description}`)
+        .setThumbnail(`${itemData.item.icon_large}`)
         .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Type', value: `${itemData.item.type}`, inline: true },
+            { name: 'Current', value: `${itemData.item.current.trend} ${itemData.item.current.price}`, inline: true },
+            { name: 'Today', value: `${itemData.item.today.trend} ${itemData.item.today.price}`, inline: true }
         )
-        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-        .setImage('https://i.imgur.com/AfFp7pu.png')
+        .addFields(
+            { name: 'Day 30', value: `${itemData.item.day30.trend} ${itemData.item.day30.change}`, inline: true },
+            { name: 'Day 90', value: `${itemData.item.day90.trend} ${itemData.item.day90.change}`, inline: true },
+            { name: 'Day 180', value: `${itemData.item.day180.trend} ${itemData.item.day180.change}`, inline: true }
+        )
         .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+        .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
         
         await interaction.reply({
             embeds: [itemEmbed]
