ea67bebe4654c9e47177a65c6437c4d9accbd675
Sinan <haxsmith77@gmail.com>
2022-11-27 05:24:30 +1100

    Updated grandexchange.js

--- src/commands/grandexchange.js
+++ src/commands/grandexchange.js
@@ -1,5 +1,9 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { AttachmentBuilder, SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+var plotly = require('plotly')("labofsinan", "mepFGawdlV2LcrAcPl2F")
+var fs = require('fs');
+
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,15 +15,52 @@ module.exports = {
         .setRequired(true)
     ),
 
+    
+
     async execute(interaction) {
         const itemId = interaction.options.getInteger('item')
-        console.log(itemId)
 
         const itemData = await fetch(`https://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=${itemId}`)
         .then((response) => response.json())
 
-        console.log(`${itemData.item.name}`)
+        const graphData = await fetch(`https://secure.runescape.com/m=itemdb_rs/api/graph/${itemId}.json`)
+        .then((response) => response.json())
+
+        const p = new Promise((resolve, reject) => {
+            const keyArray = []; 
+            const valueArray = [];
+            for (const [key, value] of Object.entries(graphData.daily)) { 
+                keyArray.push(`${key}`);
+                valueArray.push(`${value}`);
+            }
+
+            var trace = {
+              type: 'scatter',
+              x: keyArray,
+              y: valueArray,
+            };
         
+            var layout = {
+              plot_bgcolor: 'rgb(52, 54, 60)',
+              paper_bgcolor: 'rgb(52, 54, 60)',
+            };
+        
+            var chart = { data: [trace], layout: layout };
+        
+            var pngOptions = { format: 'png', width: 1000, height: 500 };
+        
+            plotly.getImage(chart, pngOptions, (err, imageStream) => {
+              if ( err ) return reject(err);
+              var fileStream = fs.createWriteStream('test.png');
+              imageStream.pipe(fileStream);
+              fileStream.on('error', reject);
+              fileStream.on('finish', resolve);
+            });
+        });
+
+        await p.then((res) => console.log("Success")).catch((error) => console.log("Error"))
+
+        const file = new AttachmentBuilder('./test.png');
 
         const itemEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
@@ -29,7 +70,7 @@ module.exports = {
         .setThumbnail(`${itemData.item.icon_large}`)
         .addFields(
             { name: 'Type', value: `${itemData.item.type}`, inline: true },
-            { name: 'Current', value: `${itemData.item.current.trend} ${itemData.item.current.price}`, inline: true },
+            { name: 'Current Price', value: `${itemData.item.current.price} GP`, inline: true },
             { name: 'Today', value: `${itemData.item.today.trend} ${itemData.item.today.price}`, inline: true }
         )
         .addFields(
@@ -37,11 +78,13 @@ module.exports = {
             { name: 'Day 90', value: `${itemData.item.day90.trend} ${itemData.item.day90.change}`, inline: true },
             { name: 'Day 180', value: `${itemData.item.day180.trend} ${itemData.item.day180.change}`, inline: true }
         )
+        .setImage('attachment://test.png')
         .setTimestamp()
         .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-        
+
         await interaction.reply({
-            embeds: [itemEmbed]
+            embeds: [itemEmbed],
+            files: [file]
         }) 
     }
 }
