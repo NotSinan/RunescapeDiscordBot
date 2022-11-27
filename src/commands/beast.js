10fa555d55d9aec3765aff44606fb43104344243
Sinan <haxsmith77@gmail.com>
2022-11-27 18:42:43 +1100

    Updated beast.js to print embed

--- src/commands/beast.js
+++ src/commands/beast.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,28 +12,36 @@ module.exports = {
     async execute(interaction) {
         const id = interaction.options.getInteger('id');
 
-        const beastData = fetch(`https://secure.runescape.com/m=itemdb_rs/bestiary/beastData.json?beastid=${id}`)
+        const beastData = await fetch(`https://secure.runescape.com/m=itemdb_rs/bestiary/beastData.json?beastid=${id}`)
         .then((response) => response.json())
+
 
         const beastEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
-        .setTitle(`${itemData.item.name}`)
-        .setURL(`https://secure.runescape.com/m=itemdb_rs/viewitem?obj=${itemData.item.id}`)
-        .setDescription(`${itemData.item.description}`)
-        .setThumbnail(`${itemData.item.icon_large}`)
+        .setTitle(`${beastData.name}`)
+        .setDescription(`${beastData.description}`) 
         .addFields(
-            { name: 'Type', value: `${itemData.item.type}`, inline: true },
-            { name: 'Current Price', value: `${itemData.item.current.price} GP`, inline: true },
-            { name: 'Today', value: `${itemData.item.today.trend} ${itemData.item.today.price}`, inline: true }
+            { name: 'Areas', value: `${beastData.areas.join("\n")}` },
+            { name: 'Combat Level', value: `${beastData.level}`, inline: true },
+            { name: 'Experience', value: `${beastData.level}`, inline: true },
+            { name: 'Magic', value: `${beastData.magic}`, inline: true },
+            { name: 'Defence', value: `${beastData.defence}`, inline: true },
+            { name: 'Ranged', value: `${beastData.ranged}`, inline: true },
+            { name: 'Attack', value: `${beastData.attack}`, inline: true }
         )
         .addFields(
-            { name: 'Day 30', value: `${itemData.item.day30.trend} ${itemData.item.day30.change}`, inline: true },
-            { name: 'Day 90', value: `${itemData.item.day90.trend} ${itemData.item.day90.change}`, inline: true },
-            { name: 'Day 180', value: `${itemData.item.day180.trend} ${itemData.item.day180.change}`, inline: true }
+            { name: 'Poisonous', value: `${beastData.poisonous}`, inline: true },
+            { name: 'Weakness', value: `${beastData.weakness}`, inline: true },
+            { name: 'Size', value: `${beastData.size}`, inline: true },
+            { name: 'Members', value: `${beastData.members}`, inline: true },
+            { name: 'Lifepoints', value: `${beastData.lifepoints}`, inline: true },
+            { name: 'Aggressive', value: `${beastData.aggressive}`, inline: true }
         )
-        .setImage(chartUrl)
         .setTimestamp()
         .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
+        await interaction.reply({
+            embeds: [beastEmbed]
+        })
     }
 }
