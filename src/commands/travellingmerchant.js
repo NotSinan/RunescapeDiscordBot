d3b80ecbd60146fe0e679fb466ac52dd352b0c51
Sinan <haxsmith77@gmail.com>
2022-11-27 23:32:40 +1100

    Code cleanup

--- src/commands/travellingmerchant.js
+++ src/commands/travellingmerchant.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const endpointRetriever = require('./singleton/endpoints')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,7 +8,7 @@ module.exports = {
     .setDescription("Get the current Travelling Merchant's Shop stock"),
 
     async execute(interaction) {
-        const data = await fetch("https://api.weirdgloop.org/runescape/tms/current?lang=en")
+        const data = await fetch(endpointRetriever.getTravellingMerchantUrl())
         .then((response) => response.json())
         await interaction.reply(data.join("\n"))
     }
