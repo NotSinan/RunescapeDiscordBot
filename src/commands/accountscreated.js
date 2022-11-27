1dd1c7592f6b9db7648644666f5f357d53957b3c
Sinan <haxsmith77@gmail.com>
2022-11-27 23:16:30 +1100

    Code cleanup

--- src/commands/accountscreated.js
+++ src/commands/accountscreated.js
@@ -1,14 +1,15 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const endpointRetriever = require('./singleton/endpoints')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("accountscreated")
-    .setDescription("Displays the number accounts created for Runescape"),
+    .setName('accountscreated')
+    .setDescription('Displays total number of RuneScape accounts created.'),
 
     async execute(interaction) {
-        const data = await fetch("https://secure.runescape.com/m=account-creation-reports/rsusertotal.ws")
+        const data = await fetch(endpointRetriever.getAccountCreatedUrl())
         .then((response) => response.json())
-        await interaction.reply(`The number of accounts created for Runescape is ${data.accountsformatted}`)
+        await interaction.reply(`${data.accountsformatted} RuneScape accounts have been created.`)
     }
 }
