b10bd93434683d5984a3f859f0772d925d91e526
Sinan <haxsmith77@gmail.com>
2022-11-27 23:32:14 +1100

    Code cleanup

--- src/commands/vos.js
+++ src/commands/vos.js
@@ -1,13 +1,14 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const endpointRetriever = require('./singleton/endpoints')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("vos")
-    .setDescription("Retrieves the voice of seren that is currently activated."),
+    .setName('vos')
+    .setDescription('Display the current Voice of Seren.'),
 
     async execute(interaction) {
-        currentVos = await fetch("https://api.weirdgloop.org/runescape/vos")
+        currentVos = await fetch(endpointRetriever.getVosUrl())
         .then((response) => response.json())
         await interaction.reply(`The current Voice of Seren is in ${currentVos.district1} and ${currentVos.district2}`)
     }
