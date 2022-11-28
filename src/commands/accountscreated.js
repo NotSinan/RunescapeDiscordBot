2e954427f4030314ac939445ab82a8a2fcd8b9d2
Sinan <haxsmith77@gmail.com>
2022-11-28 22:11:52 +1100

    Accounts created output cleanup

--- src/commands/accountscreated.js
+++ src/commands/accountscreated.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const endpointRetriever = require('./singleton/endpoints')
 
@@ -10,6 +10,17 @@ module.exports = {
     async execute(interaction) {
         const data = await fetch(endpointRetriever.getAccountCreatedUrl())
         .then((response) => response.json())
-        await interaction.reply(`${data.accountsformatted} RuneScape accounts have been created.`)
+
+        const accountsCreatedEmbed = new EmbedBuilder()
+        .setTitle('RuneScape Accounts Created')
+        .setDescription('The total RuneScape accounts created is: ')
+        .setColor('FFFFFF')
+        .setImage('https://runescape.wiki/images/Step_2_Account_creation.png?1e8c0')
+        .addFields(
+            { name: 'Accounts', value: `${data.accountsformatted}`}
+        )
+        .setTimestamp()
+        .setFooter({ text: 'Developed by Sinan', iconURL: 'https://pbs.twimg.com/tweet_video_thumb/ESoJdM8XQAAKwJ8.jpg:large' });
+        await interaction.reply({embeds: [accountsCreatedEmbed]})
     }
 }
