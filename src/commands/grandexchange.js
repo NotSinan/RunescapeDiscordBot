0641f4791b577b665a0d5c1d2f4c8dc0cc52521c
Sinan <haxsmith77@gmail.com>
2022-11-29 01:26:22 +1100

    Code clean up and function fix

--- src/commands/grandexchange.js
+++ src/commands/grandexchange.js
@@ -15,53 +15,55 @@ module.exports = {
 
     async execute(interaction) {
 
-
         interaction.deferReply()
-        const data = await Promise.all([
+
+        await Promise.all([
             fetch(endpointRetriever.getGrandExchangeUrl(interaction.options.getInteger('id'))),
             fetch(endpointRetriever.getGraphUrl(interaction.options.getInteger('id')))
-        ]).then((responses) => {
-            return Promise.all(responses.map(r => r.json()))
-        }).then((data) => {
-            return data
-        })
+        ]).then(async (responses) => {
+            if (responses[0].ok && responses[1].ok) {
+                return Promise.all(responses.map(r => r.json()))
+                .then(async ([itemData, graphData]) => {
 
+                    const keys = Object.keys(graphData.daily)
+                    const values = Object.values(graphData.daily)
+            
+                    const chart = new QuickChart();
+                    chart.setConfig({
+                    type: 'line',
+                    data: { labels: keys, datasets: [{ label:'Price', data: values }] },
+                    });
+            
+                    const chartUrl = await chart.getShortUrl();
 
-        const [itemData, graphData] = data
+                    const itemEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(`${itemData.item.name}`)
+                    .setURL(endpointRetriever.getItemImageUrl(interaction.options.getInteger('id')))
+                    .setDescription(`${itemData.item.description}`)
+                    .setThumbnail(`${itemData.item.icon_large}`)
+                    .addFields(
+                        { name: 'Type', value: `${itemData.item.type}`, inline: true },
+                        { name: 'Current Price', value: `${itemData.item.current.price} GP`, inline: true },
+                        { name: 'Today', value: `${itemData.item.today.trend} ${itemData.item.today.price}`, inline: true }
+                    )
+                    .addFields(
+                        { name: 'Day 30', value: `${itemData.item.day30.trend} ${itemData.item.day30.change}`, inline: true },
+                        { name: 'Day 90', value: `${itemData.item.day90.trend} ${itemData.item.day90.change}`, inline: true },
+                        { name: 'Day 180', value: `${itemData.item.day180.trend} ${itemData.item.day180.change}`, inline: true }
+                    )
+                    .setImage(chartUrl)
+                    .setTimestamp()
+                    .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' })
+            
+                    await interaction.editReply({
+                        embeds: [itemEmbed]
+                    })
 
-        const keys = Object.keys(graphData.daily)
-        const values = Object.values(graphData.daily)
-
-        const chart = new QuickChart();
-        chart.setConfig({
-        type: 'line',
-        data: { labels: keys, datasets: [{ label:'Price', data: values }] },
-        });
-
-        const chartUrl = await chart.getShortUrl();
-
-        const itemEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle(`${itemData.item.name}`)
-        .setURL(endpointRetriever.getItemImageUrl(interaction.options.getInteger('id')))
-        .setDescription(`${itemData.item.description}`)
-        .setThumbnail(`${itemData.item.icon_large}`)
-        .addFields(
-            { name: 'Type', value: `${itemData.item.type}`, inline: true },
-            { name: 'Current Price', value: `${itemData.item.current.price} GP`, inline: true },
-            { name: 'Today', value: `${itemData.item.today.trend} ${itemData.item.today.price}`, inline: true }
-        )
-        .addFields(
-            { name: 'Day 30', value: `${itemData.item.day30.trend} ${itemData.item.day30.change}`, inline: true },
-            { name: 'Day 90', value: `${itemData.item.day90.trend} ${itemData.item.day90.change}`, inline: true },
-            { name: 'Day 180', value: `${itemData.item.day180.trend} ${itemData.item.day180.change}`, inline: true }
-        )
-        .setImage(chartUrl)
-        .setTimestamp()
-        .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-        await interaction.editReply({
-            embeds: [itemEmbed]
+                }).catch(error => console.log(error))
+            } else {
+                await interaction.editReply("Error")
+            }
         })
     }
 }
