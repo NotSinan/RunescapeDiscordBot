fd7272b447afe4a68971b474670bb484a619b04d
Sinan <haxsmith77@gmail.com>
2022-12-06 19:23:20 +1100

    Broke beast.js, needs fixing later.

--- src/commands/beast.js
+++ src/commands/beast.js
@@ -12,35 +12,43 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply()
-        const beastData = await fetch(endpointRetriever.getBeastUrl(interaction.options.getInteger('id')))
-        .then((response) => response.json())
+        await fetch(endpointRetriever.getBeastUrl(interaction.options.getInteger('id')))
+        .then(async (response) => {
+            console.log(response.headers.get('Content-Length'))
+            if (response.ok && response.data.length > 0) {
+                response.json()
+                .then(async (beastData) => {
+                    const beastEmbed = new EmbedBuilder()
+                    .setColor('FFFFFF')
+                    .setTitle(`${beastData.name}`)
+                    .setDescription(`${beastData.description}`) 
+                    .addFields(
+                        { name: 'Areas', value: `${beastData.areas.join("\n")}` },
+                        { name: 'Combat Level', value: `${beastData.level}`, inline: true },
+                        { name: 'Experience', value: `${beastData.level}`, inline: true },
+                        { name: 'Magic', value: `${beastData.magic}`, inline: true },
+                        { name: 'Defence', value: `${beastData.defence}`, inline: true },
+                        { name: 'Ranged', value: `${beastData.ranged}`, inline: true },
+                        { name: 'Attack', value: `${beastData.attack}`, inline: true }
+                    )
+                    .addFields(
+                        { name: 'Poisonous', value: `${beastData.poisonous}`, inline: true },
+                        { name: 'Weakness', value: `${beastData.weakness}`, inline: true },
+                        { name: 'Size', value: `${beastData.size}`, inline: true },
+                        { name: 'Members', value: `${beastData.members}`, inline: true },
+                        { name: 'Lifepoints', value: `${beastData.lifepoints}`, inline: true },
+                        { name: 'Aggressive', value: `${beastData.aggressive}`, inline: true }
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-        const beastEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle(`${beastData.name}`)
-        .setDescription(`${beastData.description}`) 
-        .addFields(
-            { name: 'Areas', value: `${beastData.areas.join("\n")}` },
-            { name: 'Combat Level', value: `${beastData.level}`, inline: true },
-            { name: 'Experience', value: `${beastData.level}`, inline: true },
-            { name: 'Magic', value: `${beastData.magic}`, inline: true },
-            { name: 'Defence', value: `${beastData.defence}`, inline: true },
-            { name: 'Ranged', value: `${beastData.ranged}`, inline: true },
-            { name: 'Attack', value: `${beastData.attack}`, inline: true }
-        )
-        .addFields(
-            { name: 'Poisonous', value: `${beastData.poisonous}`, inline: true },
-            { name: 'Weakness', value: `${beastData.weakness}`, inline: true },
-            { name: 'Size', value: `${beastData.size}`, inline: true },
-            { name: 'Members', value: `${beastData.members}`, inline: true },
-            { name: 'Lifepoints', value: `${beastData.lifepoints}`, inline: true },
-            { name: 'Aggressive', value: `${beastData.aggressive}`, inline: true }
-        )
-        .setTimestamp()
-        .setFooter({ text: 'Developed by Sinan', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-        await interaction.editReply({
-            embeds: [beastEmbed]
-        })
+                    await interaction.editReply({
+                        embeds: [beastEmbed]
+                    })
+                })
+            } else if (response.data.length === 0) {
+                await interaction.editReply('Beast ID does not exist')
+            }
+        }).catch(error => console.log(error))
     }
 }
