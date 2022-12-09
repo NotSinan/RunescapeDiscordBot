14099c57820b489cb9ed321f52a46ebeee0afcbb
Sinan <haxsmith77@gmail.com>
2022-12-09 17:58:42 +1100

    Updated format.

--- src/commands/travellingmerchant.js
+++ src/commands/travellingmerchant.js
@@ -1,29 +1,29 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require("./singleton/endpoints");
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const endpointRetriever = require('./singleton/endpoints');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("travellingmerchant")
-    .setDescription("Get the current Travelling Merchant's Shop stock"),
+    data: new SlashCommandBuilder()
+        .setName('travellingmerchant')
+        .setDescription("Get the current Travelling Merchant's Shop stock"),
 
-  async execute(interaction) {
-    const data = await fetch(endpointRetriever.getTravellingMerchantUrl()).then(
-      (response) => response.json()
-    );
+    async execute(interaction) {
+        const data = await fetch(
+            endpointRetriever.getTravellingMerchantUrl()
+        ).then((response) => response.json());
 
-    travellingMerchantEmbed = new EmbedBuilder()
-      .setTitle("Travelling Merchant")
-      .setDescription("The travelling merchant is selling...")
-      .setColor("FFFFFF")
-      .addFields({ name: "Items:", value: `${data.join("\n")}` })
-      .setTimestamp()
-      .setFooter({
-        text: "Developed by Sinan",
-        iconURL: endpointRetriever.getRunescapeLogoUrl(),
-      });
+        travellingMerchantEmbed = new EmbedBuilder()
+            .setTitle('Travelling Merchant')
+            .setDescription('The travelling merchant is selling...')
+            .setColor('FFFFFF')
+            .addFields({ name: 'Items:', value: `${data.join('\n')}` })
+            .setTimestamp()
+            .setFooter({
+                text: 'Developed by Sinan',
+                iconURL: endpointRetriever.getRunescapeLogoUrl(),
+            });
 
-    await interaction.reply({ embeds: [travellingMerchantEmbed] });
-  },
+        await interaction.reply({ embeds: [travellingMerchantEmbed] });
+    },
 };
--- src/commands/vos.js
+++ src/commands/vos.js
@@ -1,31 +1,31 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require("./singleton/endpoints");
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const endpointRetriever = require('./singleton/endpoints');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("vos")
-    .setDescription("Display the current Voice of Seren."),
+    data: new SlashCommandBuilder()
+        .setName('vos')
+        .setDescription('Display the current Voice of Seren.'),
 
-  async execute(interaction) {
-    currentVos = await fetch(endpointRetriever.getVosUrl()).then((response) =>
-      response.json()
-    );
+    async execute(interaction) {
+        currentVos = await fetch(endpointRetriever.getVosUrl()).then(
+            (response) => response.json()
+        );
 
-    const districtEmbed = new EmbedBuilder()
-      .setTitle("Voice of Seren")
-      .setDescription("The current Voice of Seren is active in...")
-      .setColor("FFFFFF")
-      .addFields(
-        { name: "First district: ", value: `${currentVos.district1}` },
-        { name: "Second district: ", value: `${currentVos.district2}` }
-      )
-      .setTimestamp()
-      .setFooter({
-        text: "Developed by Sinan",
-        iconURL: endpointRetriever.getRunescapeLogoUrl(),
-      });
-    await interaction.reply({ embeds: [districtEmbed] });
-  },
+        const districtEmbed = new EmbedBuilder()
+            .setTitle('Voice of Seren')
+            .setDescription('The current Voice of Seren is active in...')
+            .setColor('FFFFFF')
+            .addFields(
+                { name: 'First district: ', value: `${currentVos.district1}` },
+                { name: 'Second district: ', value: `${currentVos.district2}` }
+            )
+            .setTimestamp()
+            .setFooter({
+                text: 'Developed by Sinan',
+                iconURL: endpointRetriever.getRunescapeLogoUrl(),
+            });
+        await interaction.reply({ embeds: [districtEmbed] });
+    },
 };
