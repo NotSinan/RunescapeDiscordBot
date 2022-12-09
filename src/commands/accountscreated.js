65544a6fbc1730f9013694dbc8b5b4d8c04ed581
Sinan <haxsmith77@gmail.com>
2022-12-09 17:41:34 +1100

    Updated to use new endpoints from singleton

--- src/commands/accountscreated.js
+++ src/commands/accountscreated.js
@@ -1,26 +1,30 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const endpointRetriever = require('./singleton/endpoints')
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const endpointRetriever = require("./singleton/endpoints");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('accountscreated')
-    .setDescription('Displays total number of RuneScape accounts created.'),
+  data: new SlashCommandBuilder()
+    .setName("accountscreated")
+    .setDescription("Displays total number of RuneScape accounts created."),
 
-    async execute(interaction) {
-        const data = await fetch(endpointRetriever.getAccountCreatedUrl())
-        .then((response) => response.json())
+  async execute(interaction) {
+    const data = await fetch(endpointRetriever.getAccountCreatedUrl()).then(
+      (response) => response.json()
+    );
 
-        const accountsCreatedEmbed = new EmbedBuilder()
-        .setTitle('RuneScape Accounts Created')
-        .setDescription('The total RuneScape accounts created is: ')
-        .setColor('FFFFFF')
-        .setImage('https://runescape.wiki/images/Step_2_Account_creation.png?1e8c0')
-        .addFields(
-            { name: 'Accounts', value: `${data.accountsformatted}`}
-        )
-        .setTimestamp()
-        .setFooter({ text: 'Developed by Sinan', iconURL: 'https://pbs.twimg.com/tweet_video_thumb/ESoJdM8XQAAKwJ8.jpg:large' });
-        await interaction.reply({embeds: [accountsCreatedEmbed]})
-    }
-}
+    const accountsCreatedEmbed = new EmbedBuilder()
+      .setTitle("RuneScape Accounts Created")
+      .setDescription("The total RuneScape accounts created is: ")
+      .setColor("FFFFFF")
+      .setImage(endpointRetriever.getAccountCreatedUrl())
+      .addFields({ name: "Accounts", value: `${data.accountsformatted}` })
+      .setTimestamp()
+      .setFooter({
+        text: "Developed by Sinan",
+        iconURL: endpointRetriever.getRunescapeLogoUrl(),
+      });
+
+    await interaction.reply({ embeds: [accountsCreatedEmbed] });
+  },
+};
