a618403d0e7fa028658805c14f00f155a5e8d08e
Sinan <haxsmith77@gmail.com>
2022-12-12 21:15:43 +1100

    Updated travelling merchant
    Added error handling

--- src/commands/travellingmerchant.js
+++ src/commands/travellingmerchant.js
@@ -1,28 +1,36 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require('./singleton/endpoints');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const endpointRetriever = require("./singleton/endpoints");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('travellingmerchant')
-        .setDescription("Get the current Travelling Merchant's Shop stock"),
+  data: new SlashCommandBuilder()
+    .setName("travellingmerchant")
+    .setDescription("Get the current Travelling Merchant's Shop stock"),
 
-    async execute(interaction) {
-        const data = await fetch(endpointRetriever.getTravellingMerchantUrl()).then((response) =>
-            response.json()
-        );
-
+  async execute(interaction) {
+    await interaction.deferReply();
+    await fetch(endpointRetriever.getTravellingMerchantUrl())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Unable to get the data");
+        }
+        return response.json();
+      })
+      .then(async (data) => {
         travellingMerchantEmbed = new EmbedBuilder()
-            .setTitle('Travelling Merchant')
-            .setDescription('The travelling merchant is selling...')
-            .setColor('FFFFFF')
-            .addFields({ name: 'Items:', value: `${data.join('\n')}` })
-            .setTimestamp()
-            .setFooter({
-                text: 'Developed by Sinan',
-                iconURL: endpointRetriever.getRunescapeLogoUrl(),
-            });
+          .setTitle("Travelling Merchant")
+          .setDescription("The travelling merchant is selling...")
+          .setColor("FFFFFF")
+          .addFields({ name: "Items:", value: `${data.join("\n")}` })
+          .setTimestamp()
+          .setFooter({
+            text: "Developed by Sinan",
+            iconURL: endpointRetriever.getRunescapeLogoUrl(),
+          });
 
-        await interaction.reply({ embeds: [travellingMerchantEmbed] });
-    },
+        await interaction.editReply({ embeds: [travellingMerchantEmbed] });
+      })
+      .catch((e) => interaction.editReply(e));
+  },
 };
