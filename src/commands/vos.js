3882979c4a58036f39fb8d0f3393a5ed2d444546
Sinan <haxsmith77@gmail.com>
2022-12-12 20:16:29 +1100

    Added Error Handling functionalities.

--- src/commands/vos.js
+++ src/commands/vos.js
@@ -1,29 +1,39 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require('./singleton/endpoints');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const endpointRetriever = require("./singleton/endpoints");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('vos')
-        .setDescription('Display the current Voice of Seren.'),
+  data: new SlashCommandBuilder()
+    .setName("vos")
+    .setDescription("Display the current Voice of Seren."),
 
-    async execute(interaction) {
-        currentVos = await fetch(endpointRetriever.getVosUrl()).then((response) => response.json());
-
+  async execute(interaction) {
+    await interaction.deferReply();
+    await fetch(endpointRetriever.getVosUrl())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Unable to get the data.");
+        }
+        return response.json();
+      })
+      .then(async (data) => {
         const districtEmbed = new EmbedBuilder()
-            .setTitle('Voice of Seren')
-            .setDescription('The current Voice of Seren is active in...')
-            .setColor('FFFFFF')
-            .addFields(
-                { name: 'First district: ', value: `${currentVos.district1}` },
-                { name: 'Second district: ', value: `${currentVos.district2}` }
-            )
-            .setTimestamp()
-            .setFooter({
-                text: 'Developed by Sinan',
-                iconURL: endpointRetriever.getRunescapeLogoUrl(),
-            });
+          .setTitle("Voice of Seren")
+          .setDescription("The current Voice of Seren is active in...")
+          .setColor("FFFFFF")
+          .addFields(
+            { name: "First district: ", value: `${data.district1}` },
+            { name: "Second district: ", value: `${data.district2}` }
+          )
+          .setTimestamp()
+          .setFooter({
+            text: "Developed by Sinan",
+            iconURL: endpointRetriever.getRunescapeLogoUrl(),
+          });
 
-        await interaction.reply({ embeds: [districtEmbed] });
-    },
+        await interaction.editReply({ embeds: [districtEmbed] });
+      })
+      .catch(async (e) => await interaction.editReply(`${e.message}`));
+  },
 };
