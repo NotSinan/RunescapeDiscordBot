65a8bfc695159a751aa5b192690832e9ddffff31
Sinan <haxsmith77@gmail.com>
2022-12-14 04:16:17 +1100

    Prettified all files

--- src/commands/travellingmerchant.js
+++ src/commands/travellingmerchant.js
@@ -1,36 +1,37 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require("./singleton/endpoints");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+const endpointRetriever = require('./singleton/endpoints')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("travellingmerchant")
-    .setDescription("Get the current Travelling Merchant's Shop stock"),
+    data: new SlashCommandBuilder()
+        .setName('travellingmerchant')
+        .setDescription("Get the current Travelling Merchant's Shop stock"),
 
-  async execute(interaction) {
-    await interaction.deferReply();
-    await fetch(endpointRetriever.getTravellingMerchantUrl())
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Unable to get the data");
-        }
-        return response.json();
-      })
-      .then(async (data) => {
-        travellingMerchantEmbed = new EmbedBuilder()
-          .setTitle("Travelling Merchant")
-          .setDescription("The travelling merchant is selling...")
-          .setColor("FFFFFF")
-          .addFields({ name: "Items:", value: `${data.join("\n")}` })
-          .setTimestamp()
-          .setFooter({
-            text: "Developed by Sinan",
-            iconURL: endpointRetriever.getRunescapeLogoUrl(),
-          });
+    async execute(interaction) {
+        await interaction.deferReply()
+        await fetch(endpointRetriever.getTravellingMerchantUrl())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Unable to get the data')
+                }
+                return response.json()
+            })
+            .then(async (data) => {
+                travellingMerchantEmbed = new EmbedBuilder()
+                    .setTitle('Travelling Merchant')
+                    .setDescription('The travelling merchant is selling...')
+                    .setColor('FFFFFF')
+                    .addFields({ name: 'Items:', value: `${data.join('\n')}` })
+                    .setTimestamp()
+                    .setFooter({
+                        text: 'Developed by Sinan',
+                        iconURL: endpointRetriever.getRunescapeLogoUrl(),
+                    })
 
-        await interaction.editReply({ embeds: [travellingMerchantEmbed] });
-      })
-      .catch((e) => interaction.editReply(e));
-  },
-};
+                await interaction.editReply({
+                    embeds: [travellingMerchantEmbed],
+                })
+            })
+            .catch((e) => interaction.editReply(e))
+    },
+}
--- src/commands/vos.js
+++ src/commands/vos.js
@@ -1,39 +1,44 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const endpointRetriever = require("./singleton/endpoints");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+const endpointRetriever = require('./singleton/endpoints')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("vos")
-    .setDescription("Display the current Voice of Seren."),
+    data: new SlashCommandBuilder()
+        .setName('vos')
+        .setDescription('Display the current Voice of Seren.'),
 
-  async execute(interaction) {
-    await interaction.deferReply();
-    await fetch(endpointRetriever.getVosUrl())
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Unable to get the data.");
-        }
-        return response.json();
-      })
-      .then(async (data) => {
-        const districtEmbed = new EmbedBuilder()
-          .setTitle("Voice of Seren")
-          .setDescription("The current Voice of Seren is active in...")
-          .setColor("FFFFFF")
-          .addFields(
-            { name: "First district: ", value: `${data.district1}` },
-            { name: "Second district: ", value: `${data.district2}` }
-          )
-          .setTimestamp()
-          .setFooter({
-            text: "Developed by Sinan",
-            iconURL: endpointRetriever.getRunescapeLogoUrl(),
-          });
+    async execute(interaction) {
+        await interaction.deferReply()
+        await fetch(endpointRetriever.getVosUrl())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Unable to get the data.')
+                }
+                return response.json()
+            })
+            .then(async (data) => {
+                const districtEmbed = new EmbedBuilder()
+                    .setTitle('Voice of Seren')
+                    .setDescription('The current Voice of Seren is active in...')
+                    .setColor('FFFFFF')
+                    .addFields(
+                        {
+                            name: 'First district: ',
+                            value: `${data.district1}`,
+                        },
+                        {
+                            name: 'Second district: ',
+                            value: `${data.district2}`,
+                        }
+                    )
+                    .setTimestamp()
+                    .setFooter({
+                        text: 'Developed by Sinan',
+                        iconURL: endpointRetriever.getRunescapeLogoUrl(),
+                    })
 
-        await interaction.editReply({ embeds: [districtEmbed] });
-      })
-      .catch(async (e) => await interaction.editReply(`${e.message}`));
-  },
-};
+                await interaction.editReply({ embeds: [districtEmbed] })
+            })
+            .catch(async (e) => await interaction.editReply(`${e.message}`))
+    },
+}
